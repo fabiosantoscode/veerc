e67344edc5a2a4ae8e78ff159928d5e6f9859675
Fábio Santos <fabiosantosart@gmail.com>
2014-04-11 13:57:26 +0100

    added some tests

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 /**
  * Main server
  */
+var websocketAsStream = require('websocket-stream')
+var Backlog = require('tailable-capped-array')
 var restify = require('restify')
 var irc = require('irc')
 var ws = require('ws')
@@ -19,28 +21,54 @@ var ircClient = new irc.Client(
     { channels: ['#mtgsapo'] }
 );
 
-function getIrcClient(socket) { /* TODO */ return ircClient }
+ircClient.on('raw', function (message) {
+    backlog.push(JSON.stringify(message))
+})
+
+ircClient.on('error', console.error.bind(console))
+
+var backlog = new Backlog(10 /* backlog size */)
+var backlogStream = backlog.createReadStream()
 
 var sockServer = new ws.Server({ server: pureServer })
 
 sockServer.on('connection', function(sock) {
-    var ircClient = getIrcClient(sock)
-    ircClient.on('raw', function (message) {
-        sock.send(JSON.stringify(message))
-    })
-    sock.on('message', function (msg) {
+    sock = websocketAsStream(sock)
+
+    backlogStream.pipe(sock)
+    sock.on('data', function (msg) {
         try {
             msg = JSON.parse(msg)
-        } catch(e) { sock.send(JSON.stringify({ error: 'Dude, wrong JSON ' + e })) }
-        if (msg.type === 'message') {
-            ircClient.say(msg.to, msg.content)
-        } else if (msg.type === 'join') {
-            ircClient.join(msg.chan)
-        } else if (msg.type === 'part') {
-            ircClient.part(msg.chan)
+        } catch(e) {
+            backlogStream.push(JSON.stringify({ error: 'Dude, wrong JSON ' + e }))
         }
+
+        fail: {
+            if (msg.type === 'message') {
+                if (msg.to && 'content' in msg) {
+                    ircClient.say(msg.to, msg.content)
+                }
+            } else if (msg.type === 'join') {
+                if (msg.chan) {
+                    ircClient.join(msg.chan)
+                }
+            } else if (msg.type === 'part') {
+                if (msg.chan) {
+                    ircClient.part(msg.chan)
+                }
+            } else { break fail; }
+            return;
+        }
+
+        backlogStream.push(JSON.stringify({ error: 'Dude, you sent a bad message and shite' }))
     })
+
+    sock.on('error', console.error.bind(console))
 })
 
-server.listen(9000)
-
+if (!module.parent) {  // Started from console
+    server.listen(9000)
+} else {  // required
+    module.exports.sockServer = sockServer
+    module.exports.server = server
+}
